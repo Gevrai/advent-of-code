7ad6df549bc84cf635472569272b38f6672066c0
Gevrai Jodoin-Tremblay <gejo@kaloom.com>
2020-12-03 00:10:07 -0500

    day3

---
+++ 2020/day3/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"advent-of-code-2020/utils"
+)
+
+func main() {
+	input := utils.ReadInputFileRelative()
+
+	println("Part 1:", skiDown(input, 3, 1))
+
+	println("Part 2:", skiDown(input, 1, 1)*
+		skiDown(input, 3, 1)*
+		skiDown(input, 5, 1)*
+		skiDown(input, 7, 1)*
+		skiDown(input, 1, 2),
+	)
+}
+
+func skiDown(input []string, x, y int) (trees int) {
+	var i, j int
+	for {
+		i = (i + x) % len(input[0])
+		j += y
+
+		if j >= len(input) {
+			return trees
+		}
+		if input[j][i] == '#' {
+			trees++
+		}
+	}
+}
