efa71443d1d3699a6f5ca64eea7591ea8d9ddd2f
Gevrai Jodoin-Tremblay <gejo@kaloom.com>
2020-12-05 22:23:51 -0500

    day5, but golfing with bash

---
+++ 2020/day5/main.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+echo "$((2#$(sed -E 's/B|R/1/g;s/F|L/0/g' input.txt|sort|tail -1)))"
+for i in $(sed -E 's/B|R/1/g;s/F|L/0/g' input.txt|sort);{ [[ $[$p+2] == $[2#$i] ]] && echo $[$p+1];p=$[2#$i];}
